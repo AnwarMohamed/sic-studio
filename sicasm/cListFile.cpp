be764c483febe72d3d264d248582bc89c21ac8e1
AnwarMohamed <anwarelmakrahy@gmail.com>
2014-11-05 03:31:30 +0200

    Handling Opcodes

--- sicasm/cListFile.cpp
+++ sicasm/cListFile.cpp
@@ -8,6 +8,7 @@
 #define ERROR_MISSING_OPCODE  "missing operation code"
 #define ERROR_UNRECOG_OPCODE  "unrecognized operation code"
 #define ERROR_MISSING_OPERAND "missing or misplaced operand in instruction"
+#define ERROR_ILLEGAL_OPERAND "illegal operand in instruction"
 #define ERROR_UNDEFINED_SYM   "undefined symbol in operand"
 #define ERROR_MISSING_START   "missing or misplaced start statement"
 #define ERROR_ILLEGAL_START   "illegal operand in start statement"
@@ -27,6 +28,22 @@
 
 cListFile::cListFile(char* filename) : cSourceFile(filename) {
 	_end_set = false;
+	_start_set = false;
+
+	ListFileOpCode* opcode;
+	for (int i = 0; i < 59; ++i)
+	{
+		opcode = new ListFileOpCode;
+		zero(opcode, sizeof(ListFileOpCode));
+
+		opcode->mnemonic = opcodes_mnemonic[i];
+		opcode->opcode = opcodes_format[i][0];
+		opcode->operands = opcodes_format[i][1];
+		opcode->register_based = opcodes_format[i][2] ? true : false;
+
+		_opcodes_table[opcodes_mnemonic[i]] = opcode;
+	}
+
 	is_ready = is_ready ? parse_sourcefile_lines() : false;
 }
 
@@ -37,9 +54,9 @@ void cListFile::construct_symbol_table() {
 		listfile_line = new ListFileLine;
 		zero(listfile_line, sizeof(ListFileLine));
 
-		if (_sourcefile_lines[i]->directive.size()) {
-			if (_symbols_table.count(_sourcefile_lines[i]->directive) == 0) {
-				_symbols_table[_sourcefile_lines[i]->directive] = i;
+		if (_sourcefile_lines[i]->label.size()) {
+			if (_symbols_table.count(_sourcefile_lines[i]->label) == 0) {
+				_symbols_table[_sourcefile_lines[i]->label] = i;
 			}
 			else {
 				listfile_line->errors.push_back(ERROR_DUPLICATE_LABEL);
@@ -59,6 +76,8 @@ bool cListFile::parse_instructions() {
 		if (is_hex_number(_sourcefile_lines[0]->operand)) {
 			_start_address = _current_address = 
 				hex_to_int((char*)_sourcefile_lines[0]->operand.c_str());
+			_listfile_lines[0]->address = _start_address;
+			_start_set = true;
 		}
 		else {
 			_start_address = _current_address = 0;
@@ -70,15 +89,17 @@ bool cListFile::parse_instructions() {
 		_listfile_lines[0]->errors.push_back(ERROR_MISSING_START);
 	}
 
-	for (int i = 1; i < _sourcefile_lines.size(); ++i) {
+	map<string, ListFileOpCode*>::iterator opcode_table_it;
+
+	for (int i = _start_set?1:0; i < (int)_sourcefile_lines.size(); ++i) {
 		if (_sourcefile_lines[i]->instruction.size()) {
 
-			/* Handling START Operation */
+			/* Handling START Directive */
 			if (_sourcefile_lines[i]->instruction == "START") {
 				_listfile_lines[i]->errors.push_back(ERROR_DUPLICATE_START);
 			}
 
-			/* Handling WORD Operation */
+			/* Handling WORD Directive */
 			else if (_sourcefile_lines[i]->instruction == "WORD") {
 				_listfile_lines[i]->address = _current_address;
 				_current_address += 3;
@@ -88,7 +109,7 @@ bool cListFile::parse_instructions() {
 				}
 			}
 
-			/* Handling BYTE Operation */
+			/* Handling BYTE Directive */
 			else if (_sourcefile_lines[i]->instruction == "BYTE") {
 				_listfile_lines[i]->address = _current_address;
 
@@ -103,7 +124,7 @@ bool cListFile::parse_instructions() {
 				}
 			}
 
-			/* Handling RESW Operation */
+			/* Handling RESW Directive */
 			else if (_sourcefile_lines[i]->instruction == "RESW") {
 				_listfile_lines[i]->address = _current_address;
 
@@ -117,7 +138,7 @@ bool cListFile::parse_instructions() {
 				}
 			}
 
-			/* Handling RESB Operation */
+			/* Handling RESB Directive */
 			else if (_sourcefile_lines[i]->instruction == "RESB") {
 				_listfile_lines[i]->address = _current_address;
 
@@ -131,7 +152,96 @@ bool cListFile::parse_instructions() {
 				}
 			}
 
+			/* Handling END Directive */
+			else if (_sourcefile_lines[i]->instruction == "END") {
+				_listfile_lines[i]->address = _current_address;
+				_current_address += 1;
+				_end_set = true;
 
+				if (_symbols_table.count(_sourcefile_lines[i]->operand) != 1) {
+					_listfile_lines[i]->errors.push_back(ERROR_UNDEFINED_SYM);					
+				}
+			}
+
+			/* Handling Opcodes */
+			else if ((opcode_table_it = _opcodes_table.find(
+				_sourcefile_lines[i]->instruction)) != _opcodes_table.end()) {
+				_listfile_lines[i]->address = _current_address;
+				_current_address += 3;
+
+				_listfile_lines[i]->object_code |= opcode_table_it->second->opcode << 16;
+
+				switch (opcode_table_it->second->operands)
+				{
+				case 0:
+					if (_sourcefile_lines[i]->operand.size()) {
+						_listfile_lines[i]->errors.push_back(
+							ERROR_ILLEGAL_OPERAND);
+					}
+					break;
+				case 1:
+					if (_sourcefile_lines[i]->operand.size()) {
+						if (_sourcefile_lines[i]->operand.find(',') != -1) {
+							if (_sourcefile_lines[i]->operand.find_last_of(',')
+								!= _sourcefile_lines[i]->operand.find(',') ||
+								_sourcefile_lines[i]->operand.find(',') + 2
+								!= _sourcefile_lines[i]->operand.size() ||
+								_sourcefile_lines[i]->operand[
+									_sourcefile_lines[i]->operand.find(',')+1]
+								!= 'X') {
+								_listfile_lines[i]->errors.push_back(
+									ERROR_ILLEGAL_OPERAND);
+							}
+						}
+						else if (_symbols_table.count(
+							_sourcefile_lines[i]->operand) != 1) {
+							_listfile_lines[i]->errors.push_back(
+								ERROR_UNDEFINED_SYM);
+						}
+						else {
+							_listfile_lines[i]->object_code |= _listfile_lines[
+								_symbols_table[
+									_sourcefile_lines[i]->operand]]->address;
+						}
+					}
+					else {
+						_listfile_lines[i]->errors.push_back(
+							ERROR_MISSING_OPERAND);
+					}
+					break;
+				case 2:
+					if (_sourcefile_lines[i]->operand.find_last_of(',') != -1 &&
+						_sourcefile_lines[i]->operand.find_last_of(',')
+						== _sourcefile_lines[i]->operand.find(',')) {
+						if (_sourcefile_lines[i]->instruction == "SHIFTL" ||
+							_sourcefile_lines[i]->instruction == "SHIFTR") {
+
+						}
+						else {
+
+						}
+					}	
+					else {
+						_listfile_lines[i]->errors.push_back(
+							ERROR_ILLEGAL_OPERAND);
+					}
+					break;
+				}
+
+	
+
+			}
+
+			/* Handling Unknown Opcodes */
+			else {
+				_listfile_lines[i]->address = _current_address;
+				_listfile_lines[i]->errors.push_back(ERROR_UNRECOG_OPCODE);
+			}
+
+
+			if (_end_set && _sourcefile_lines[i]->instruction != "END") {
+				_listfile_lines[i]->errors.push_back(ERROR_AFTER_END);
+			}
 		}
 	}
 
@@ -145,7 +255,7 @@ bool cListFile::parse_sourcefile_lines() {
 }
 
 bool cListFile::is_word_str(string& str) {
-	return is_numeric(str) && (str_to_int((char*)str.c_str()) != 1 ? true : false);
+	return is_numeric(str) && (str_to_int((char*)str.c_str()) != -1 ? true : false);
 }
 
 bool cListFile::is_numeric(string& str) {
@@ -169,6 +279,28 @@ int cListFile::hex_to_int(char* hex) {
 	int result;
 	sscanf_s(hex, "%x", &result);
 	return (unsigned int)result > 0x0000ffff ? -1 : result;
+}
+
+void cListFile::print_listfile() {
+	ListFileLine* listfile_line;
+	SourceFileLine* sourcefile_line;
+
+	for (int i = 0; i < (int)_listfile_lines.size(); ++i) {
+		listfile_line = _listfile_lines[i];
+		sourcefile_line = _sourcefile_lines[i];
+
+		printf("%04x %06x %-9s %-8s %-9s %-10s\n",
+			listfile_line->address,
+			(unsigned int)listfile_line->object_code & 0x00ffffff,
+			sourcefile_line->label.c_str(),
+			sourcefile_line->instruction.c_str(),
+			sourcefile_line->operand.c_str(),
+			sourcefile_line->comment.c_str());
+
+		for (int j = 0; j < (int)listfile_line->errors.size(); ++j) {
+			printf(" **** %s\n", listfile_line->errors[j].c_str());
+		}
+	}
 }
 
 cListFile::~cListFile() {
--- sicasm/cListFile.h
+++ sicasm/cListFile.h
@@ -4,20 +4,148 @@
 
 using namespace std;
 
+static char* opcodes_mnemonic[59] = {
+	"ADD",
+	"ADDF",
+	"ADDR",
+	"AND",
+	"CLEAR",
+	"COMP",
+	"COMPF",
+	"COMPR",
+	"DIV",
+	"DIVF",
+	"DIVR",
+	"FIX",
+	"FLOAT",
+	"HIO",
+	"J",
+	"JEQ",
+	"JGT",
+	"JLT",
+	"JSUB",
+	"LDA",
+	"LDB",
+	"LDCH",
+	"LDF",
+	"LDL",
+	"LDS",
+	"LDT",
+	"LDX",
+	"LPS",
+	"MUL",
+	"MULF",
+	"MULR",
+	"NORM",
+	"OR",
+	"RD",
+	"RMO",
+	"RSUB",
+	"SHIFTL",
+	"SHIFTR",
+	"SIO",
+	"SSK",
+	"STA",
+	"STB",
+	"STCH",
+	"STF",
+	"STI",
+	"STL",
+	"STS",
+	"STSW",
+	"STT",
+	"STX",
+	"SUB",
+	"SUBF",
+	"SUBR",
+	"SVC",
+	"TD",
+	"TIO",
+	"TIX",
+	"TIXR",
+	"WD",
+};
+
+static unsigned char opcodes_format[59][3] = {
+	{ 0x18, 1, 0},
+	{ 0x58, 1, 0},
+	{ 0x90, 2, 1},
+	{ 0x40, 1, 0},
+	{ 0xB4, 1, 1},
+	{ 0x28, 1, 0},
+	{ 0x88, 1, 0},
+	{ 0xA0, 2, 1},
+	{ 0x24, 1, 0},
+	{ 0x64, 1, 0},
+	{ 0x9C, 2, 1},
+	{ 0xC4, 0, 0},
+	{ 0xC0, 0, 0},
+	{ 0xF4, 0, 0},
+	{ 0x3C, 1, 0},
+	{ 0x30, 1, 0},
+	{ 0x34, 1, 0},
+	{ 0x38, 1, 0},
+	{ 0x48, 1, 0},
+	{ 0x00, 1, 0},
+	{ 0x68, 1, 0},
+	{ 0x50, 1, 0},
+	{ 0x70, 1, 0},
+	{ 0x08, 1, 0},
+	{ 0x6C, 1, 0},
+	{ 0x74, 1, 0},
+	{ 0x04, 1, 0},
+	{ 0xD0, 1, 0},
+	{ 0x20, 1, 0},
+	{ 0x60, 1, 0},
+	{ 0x98, 2, 1},
+	{ 0xC8, 0, 0},
+	{ 0x44, 1, 0},
+	{ 0xD8, 1, 0},
+	{ 0xAC, 2, 1},
+	{ 0x4C, 0, 0},
+	{ 0xA4, 2, 1},
+	{ 0xA8, 2, 1},
+	{ 0xF0, 0, 0},
+	{ 0xEC, 1, 0},
+	{ 0x0C, 1, 0},
+	{ 0x78, 1, 0},
+	{ 0x54, 1, 0},
+	{ 0x80, 1, 0},
+	{ 0xD4, 1, 0},
+	{ 0x14, 1, 0},
+	{ 0x7C, 1, 0},
+	{ 0xE8, 1, 0},
+	{ 0x84, 1, 0},
+	{ 0x10, 1, 0},
+	{ 0x1C, 1, 0},
+	{ 0x5C, 1, 0},
+	{ 0x94, 2, 1},
+	{ 0xB0, 1, 0},
+	{ 0xE0, 1, 0},
+	{ 0xF8, 0, 0},
+	{ 0x2C, 1, 0},
+	{ 0xB8, 1, 1},
+	{ 0xDC, 1, 0},
+};
+
 enum ListFileErrors {
 	
 };
 
-struct ListFileLine
-{
-    int index;
+struct ListFileLine {
+    int object_code;
     int address;
-    string label;
-    string instruction;
-    string operands;
-    string comment;
     vector<string> errors;
 };
+
+struct ListFileOpCode {
+	string mnemonic;
+	char opcode;
+	char format;
+	bool register_based;
+	char operands;
+};
+
 
 class cListFile: public cSourceFile
 {
@@ -25,12 +153,15 @@ public:
     cListFile(char* filename);
     ~cListFile();
 
+	void print_listfile();
+
 private:
 	bool parse_sourcefile_lines();
 	int hex_to_int(char* hex);
 	int str_to_int(char* str);
 
 	bool _end_set;
+	bool _start_set;
 	int _current_address;
 
 	bool is_numeric(string& str);
@@ -42,6 +173,7 @@ private:
 protected:
 	int _start_address;
 	map<string, int> _symbols_table;
+	map<string, ListFileOpCode*> _opcodes_table;
 	vector<ListFileLine*> _listfile_lines;
 };
 
